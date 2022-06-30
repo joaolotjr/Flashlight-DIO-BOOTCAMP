4809a2bdd99da3f3e925a50228517672f6523ac5
joaolotjr <joaolotjr@gmail.com>
2022-06-30 18:20:54 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {} from 'react-native';
+
+
+const App = () => {
+
+    return <View />;
+};
+
+export default App;
+
+const style = StyleSheet.create({
+
+
+  
+})
